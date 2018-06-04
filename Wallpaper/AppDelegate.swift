f22318b339d93b01ea01d3c3be431cc80477e928
ShuLiang <shuliang@live.cn>
2018-06-04 15:25:00 +0800

    Initial Commit

---
+++ Wallpaper/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Wallpaper
+//
+//  Created by sl on 2018/6/4.
+//  Copyright © 2018 shuliang. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
