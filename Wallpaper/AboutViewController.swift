08e241d5f0fdff07cd35638e1987b0029ad7542a
ShuLiang <shuliang@live.cn>
2018-06-07 19:23:16 +0800

    About page content

--- Wallpaper/AboutViewController.swift
+++ Wallpaper/AboutViewController.swift
@@ -11,40 +11,85 @@ import Cocoa
 class AboutViewController: NSViewController {
 
     @IBOutlet weak var versionLabel: NSTextField!
-    @IBOutlet weak var authorLabel: NSTextField!
-    @IBOutlet weak var creditTextField: NSTextField!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         configureUI()
     }
     
+    override func viewWillAppear() {
+        super.viewWillAppear()
+        view.window?.styleMask.remove(.resizable)
+    }
+    
     private func configureUI() {
         let version = Bundle.main.infoDictionary?["CFBundleShortVersionString"] as? String ?? "1.0"
-        versionLabel.stringValue = "Version \(version)"
-        
-        authorLabel.isEditable = false
-        let authorStr = NSMutableAttributedString(string: "Developed by ")
-        guard let github = URL(string: "https://github.com/shuliang") else { return }
-        let author = NSAttributedString.wp_linkedString(text: "@shuliang", url: github, font: NSFont.systemFont(ofSize: 12, weight: .light))
-        authorStr.append(author)
-        authorLabel.attributedStringValue = authorStr
+        versionLabel.stringValue = "Version \(version)"        
     }
 }
 
-extension NSAttributedString {
-    static func wp_linkedString(text: String, url: URL, font: NSFont = NSFont.systemFont(ofSize: 13)) -> NSAttributedString {
-        let attrStr = NSMutableAttributedString(string: text)
-        let range = NSRange(location: 0, length: attrStr.length)
-        let attrs: [NSAttributedStringKey: Any] = [
-            .link: url,
-            .font: font,
-            .foregroundColor: NSColor.blue
-        ]
-        attrStr.beginEditing()
-        attrStr.addAttributes(attrs, range: range)
-        attrStr.endEditing()
+// MARK: - SubstringLinkedTextField
+
+// workaround, see more:
+// https://stackoverflow.com/questions/38340282/simple-clickable-link-in-cocoa-and-swift
+// A text field that can contain a hyperlink within a range of characters in the text.
+@IBDesignable
+public class SubstringLinkedTextField: NSTextField {
+    // the URL that will be opened when the link is clicked.
+    public var link: String = ""
+    @available(*, unavailable, message: "This property is reserved for Interface Builder. Use 'link' instead.")
+    @IBInspectable public var HREF: String {
+        get {
+            return self.link
+        }
+        set {
+            self.link = newValue
+            self.needsDisplay = true
+        }
+    }
+    
+    // the substring within the field's text that will become an underlined link. if empty or no match found, the entire text will become the link.
+    public var linkText: String = ""
+    @available(*, unavailable, message: "This property is reserved for Interface Builder. Use 'linkText' instead.")
+    @IBInspectable public var LinkText: String {
+        get {
+            return self.linkText
+        }
+        set {
+            self.linkText = newValue
+            self.needsDisplay = true
+        }
+    }
+    
+    override public func awakeFromNib() {
+        super.awakeFromNib()
         
-        return attrStr
+        self.allowsEditingTextAttributes = true
+        self.isSelectable = true
+        
+        let url = URL(string: self.link)
+        let attributes: [NSAttributedStringKey: Any] = [
+            .link: url as Any,
+            .font: self.font as Any
+        ]
+        let attributedStr = NSMutableAttributedString(string: self.stringValue)
+        
+        if self.linkText.count > 0 {
+            if let range = self.stringValue.range(of: self.linkText) {
+                let nsRange = self.stringValue.nsRange(from: range)
+                attributedStr.setAttributes(attributes, range: nsRange)
+            } else {
+                attributedStr.setAttributes(attributes, range: NSMakeRange(0, self.stringValue.count))
+            }
+        } else {
+            attributedStr.setAttributes(attributes, range: NSMakeRange(0, self.stringValue.count))
+        }
+        self.attributedStringValue = attributedStr
+    }
+}
+
+extension StringProtocol where Index == String.Index {
+    func nsRange(from range: Range<Index>) -> NSRange {
+        return NSRange(range, in: self)
     }
 }
